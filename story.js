280d6d7ba34e15c2a36cfdcf9166aa748cc94565
treeebonies <treeebonies@gmail.com>
2022-01-09 20:19:37 -0500

    modified story

--- story.js
+++ story.js
@@ -1,3 +1,4 @@
-timmy, dan and john are besties
-they went to the park one day
+timmy, dan and john are besties.
+they went to the park one day.
 timmy ate cotton candy and john played with a nearby dog.
+danny ate too much ice cream and got brain freeze so he had to go home.
