e050f6da2d8846842729521f1c871f4db9d5b5fd
treeebonies <treeebonies@gmail.com>
2022-01-09 20:24:09 -0500

    timmy added

--- story.js
+++ story.js
@@ -2,3 +2,4 @@ timmy, dan and john are besties.
 they went to the park one day.
 timmy ate cotton candy and john played with a nearby dog.
 danny ate too much ice cream and got brain freeze so he had to go home.
+John was chased down by the dog.
